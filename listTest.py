5f84a762923a0d92b00fdd8b150299b0e53b9f52
irvin <irvinhomem@gmail.com>
2015-04-08 22:44:37 +0200

    3rd commit

--- listTest.py
+++ listTest.py
@@ -6,7 +6,7 @@ fruits =['avocados','bananas','oranges','grapes','mangos']
 
 vegetables = ['carrots','potatoes','onions','leeks','celery']
 
-inventory = ['shoes']
+inventory = ['shoes','socks']
 
 for i, value in enumerate(fruits):
     print(i, value)
