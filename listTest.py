d50416e8eba8ec1ac1f83751a28ec9d35ff12afb
irvin <irvinhomem@gmail.com>
2015-04-08 22:35:01 +0200

    First commit

---
+++ listTest.py
@@ -0,0 +1,25 @@
+__author__ = 'irvin'
+
+myMatrix = [[11,12,13],[21,22,23],[31,32,33]]
+
+fruits =['avocados','bananas','oranges','grapes','mangos']
+
+vegetables = ['carrots','potatoes','onions','leeks','celery']
+
+for i, value in enumerate(fruits):
+    print(i, value)
+
+######
+
+for row in myMatrix:
+    for i, value in enumerate(row):
+        print(i, value,)
+    print()
+
+######
+
+for fru, veg in zip(fruits, vegetables):
+    if fru < veg:
+        print(fru, "are better than", veg)
+    else:
+        print(veg, "are better than", fru)
